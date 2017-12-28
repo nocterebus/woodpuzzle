da8da4ce4cbb46ef01475fddb9c6e98edce5460b
nocterebus <justeny128@gmail.com>
2017-12-27 19:52:51 -0500

    complete Lshape and test print

--- solve.cpp
+++ solve.cpp
@@ -5,7 +5,44 @@ By Justen Yeung
 */
 #include "solve.h"
 
-int main (intargc, char** argv){
 
+void pout(std::vector<std::vector<std::vector<std::string>>> input){
+ for (int z = 0; z < 3; ++z){
+   if (z == 0){
+     std::cout<<"bottom layer\n";
+   }
+   else if (z == 1){
+     std::cout<<"middle layer\n";
+   }
+   else {
+     std::cout<<"top layer\n";
+   }
+   for (int y = 2; y >= 0; --y){
+     for (int x = 0; x < 3; ++x){
+       std::cout<<input[z][y][x];
+     }
+     std::cout<<"\n";
+   }
+   std::cout<<"\n";
+ }
+}
+int main (int argc, char** argv){
+  std::vector<std::string> emptycolumn = {"0", "0", "0"};
+  std::vector<std::vector<std::string>> emptylayer(3,emptycolumn);
+  std::vector<std::vector<std::vector<std::string>>> cube(3, emptylayer);
+  Lshape L;
+  pout(cube);
+  std::cout<<L.size<<"\n";
+  std::cout<<L.shape[0][1]<<"\n";
+  int dz,dy = 0;
+  int dx = 1;
+  for (int block = 0; block < L.size; ++block){
+    dz += L.shape.at(block).at(0);
+    dy += L.shape.at(block)[1];
+    dx += L.shape.at(block)[2];
+    cube[dz][dy][dx] = L.name;
+  }
+  pout(cube);
+  std::cout<<L.name;
   return 0;
 }
--- solve.h
+++ solve.h
@@ -15,29 +15,69 @@ By Justen Yeung
 
 //general class
 class piece{
-//initiate variables
-public:
-  int size;
-  //next block location relative to current (zyx)(up forward right)
-  std::vector<std::vector<int> shape(3,std::vector<int>(3,0)));
-  //orientation in space (6 axis, 24 possible orientations)
-  std::vector<int> orientation;
-  //block selected as center of piece to rotate around and place
-  int center;
 private:
 protected:
+//initiate variables
+public:
+  std::string name;
+  int size;
+  //hard set block locations kept in case of need of reset
+  std::vector<std::vector<int>> Shape;
+  //next block location relative to current (zyx)(up away right) alterable
+  std::vector<std::vector<int>> shape;
+  //orientation in space (6 axis, 24 possible orientations)(a b c, d e f, g h i)
+  std::vector<int> orientation = {1,0,0,0,1,0,0,0,1};
+  //block selected as center of piece to rotate around and place
+  int center = 0;
+
+  void reorient(std::vector<int>);
+  void recenter();
 };
+
+void piece::reorient(std::vector<int> axis){
+  //ensure input is correct
+  if (axis.size() != 9){
+    std::cout<<"Unable to reorient with given orientation \n";
+    return;
+  }
+  //ensure size of piece is correct
+  if(size == 4 or size == 3){
+    shape = Shape;
+    int c = center;
+    //iterate through piece blocks starting from current center
+    for (int i = 0; i < size; ++ i){
+      if (c >= size){
+        c = 0;
+      }
+      //change next block location depending on axis orientation
+      for (int i = 0; i < 3; ++ i){
+        //shape[c] =
+      }
+      ++ c;
+    }
+  }
+  else{
+    std::cout<<"Attempted to reorient a piece that was not of size 4 or 3 \n";
+    return;
+  }
+}
+
+void piece::recenter(){
+
+}
 
 //specific classes
 class Lshape : public piece{
-  public:
-    //null constructor
-    Lshape(){
-      size = 4;
-      //starting from block 0
-      shape =[[0,-1,0],[1,0,0],[1,0,0],[-2,1,0]];
-    };
+public:
+  //null constructor
+  Lshape(){
+    name = "L";
+    size = 4;
+    //hard set block locations kept in case of need of reset
+    Shape = {{0,1,0},{1,0,0},{1,0,0},{-2,-1,0}};
+    //starting from block 0 at [1,1,1] orientation
+    shape = {{0,1,0},{1,0,0},{1,0,0},{-2,-1,0}};
+  };
 };
-
 
 #endif
