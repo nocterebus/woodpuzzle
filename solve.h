88d301c0e1d2558970f4230f786208723ba8087c
nocterebus <justeny128@gmail.com>
2018-01-04 17:52:04 -0500

    completed adding piece.reorient function

--- solve.h
+++ solve.h
@@ -24,17 +24,17 @@ public:
   //hard set block locations kept in case of need of reset
   //starting from block 0 at [1,0,0,0,1,0,0,0,1] orientation
   std::vector<std::vector<int>> Shape;
-  //next block location relative to current (zyx)(up away right) alterable
+  //next block location relative to current (xyz)(up away right) alterable
   std::vector<std::vector<int>> shape;
-  //orientation in space (zyx, 24 possible orientations)(a b c, d e f, g h i)
+  //orientation in space (xyz, 24 possible orientations)(a b c, d e f, g h i)
   std::vector<int> orient_now = {1,0,0,0,1,0,0,0,1};
   //right hand rule rotations around stated axis (counter clockwise 90degrees)
-  std::vector<int> reorient_z = {1,0,0,0,0,-1,0,1,0};
+  std::vector<int> reorient_x = {1,0,0,0,0,-1,0,1,0};
   std::vector<int> reorient_y = {0,0,1,0,1,0,-1,0,0};
-  std::vector<int> reorient_x = {0,-1,0,1,0,0,0,0,1};
-  int z_rotations = 0;
-  int y_rotations = 0;
+  std::vector<int> reorient_z = {0,-1,0,1,0,0,0,0,1};
   int x_rotations = 0;
+  int y_rotations = 0;
+  int z_rotations = 0;
   //block selected as center of piece to rotate around and place...will be initially as 0 to start, later implement random center
   int center = 0;
   //amount of times center of piece has been changed
@@ -49,7 +49,7 @@ public:
 //reset stats on piece if all placement combinations tested on current placement block
 void piece::reset(){
   shape = Shape;
-  z_rotations, y_rotations, x_rotations, center, recenters = 0;
+  x_rotations, y_rotations, z_rotations, center, recenters = 0;
   orient_now = {1,0,0,0,1,0,0,0,1};
 }
 //change the block of piece designated as center of piece
@@ -61,7 +61,7 @@ void piece::recenter(){
   if (recenters >= size){
     std::cout<<"piece does not fit here. moving to another spot\n";
     //reset stats on piece
-    piece.reset();
+    reset();
   }
 }
 //rotate piece along an axis to a rotation that has not been checked yet
@@ -70,8 +70,8 @@ void piece::reorient(){
   //if z rotated 4 times then piece will not fit in this spot and has made all possible rotations
   if (z_rotations >= size){
     //back at original orientation along z axis
-    std::cout<<piece.name<<" doesn't fit. changing center.\n";
-    piece.recenter();
+    std::cout<<name<<" doesn't fit. changing center.\n";
+    recenter();
     return;
   }
   //if back at original orientation along y axis, rotate once along z axis
@@ -79,8 +79,10 @@ void piece::reorient(){
     //back at original orientation along y axis. can reset Y_rotations count to 0
     y_rotations = 0;
     //rotate along z_axis
-    for (int nblock = 0; nblock < piece.size; ++b){
-      shape[nblock] = {((shape[nblock][0])+(shape[nblock][1])+(shape[nblock][2]),(),())}
+    for (int nblock = 0; nblock < size; ++nblock){
+      shape[nblock] = {((shape[nblock][0])*0+(shape[nblock][1])*-1+(shape[nblock][2])*0),//change in z position to next piece
+                        ((shape[nblock][0]*1)+(shape[nblock][1])*0+(shape[nblock][2])*0),//change in y position to next piece
+                        ((shape[nblock][0]*0)+(shape[nblock][1])*0+(shape[nblock][2])*1)};//change in x position to next piece
     }
     //increase z_rotations count by 1
     ++z_rotations;
@@ -90,14 +92,22 @@ void piece::reorient(){
     //back at original orientation along x axis. can reset x_rotations count to 0
     x_rotations = 0;
     //rotate along y_axis
-
+    for (int nblock = 0; nblock < size; ++nblock){
+      shape[nblock] = {((shape[nblock][0])*0+(shape[nblock][1])*0+(shape[nblock][2])*1),//change in z position to next piece
+                        ((shape[nblock][0]*0)+(shape[nblock][1])*1+(shape[nblock][2])*0),//change in y position to next piece
+                        ((shape[nblock][0]*-1)+(shape[nblock][1])*0+(shape[nblock][2])*0)};//change in x position to next piece
+    }
     //increase y_rotations count by 1
     ++y_rotations;
   }
   //else rotate piece along x_axis
   else {
     //rotate along x_axis
-
+    for (int nblock = 0; nblock < size; ++nblock){
+      shape[nblock] = {((shape[nblock][0])*1+(shape[nblock][1])*0+(shape[nblock][2])*0),//change in z position to next piece
+                        ((shape[nblock][0]*0)+(shape[nblock][1])*0+(shape[nblock][2])*-1),//change in y position to next piece
+                        ((shape[nblock][0]*0)+(shape[nblock][1])*1+(shape[nblock][2])*0)};//change in x position to next piece
+    }
     //increase x_rotations count by 1
     ++x_rotations;
   }
@@ -110,10 +120,10 @@ public:
   Lshape(){
     name = "L";
     size = 4;
-    //hard set block locations kept in case of need of reset
-    Shape = {{0,1,0},{1,0,0},{1,0,0},{-2,-1,0}};
+    //hard set block next locations kept in case of need of reset{dx, dy, dz}
+    Shape = {{0,1,0},{0,0,1},{0,0,1},{0,-1,-2}};
     //starting from block 0 at [1,0,0,0,1,0,0,0,1] orientation
-    shape = {{0,1,0},{1,0,0},{1,0,0},{-2,-1,0}};
+    shape = {{0,1,0},{0,0,1},{0,0,1},{0,-1,-2}};
   };
 };
 
